b6068dbb6ba3762f78083ea5b691026860424b7c
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-03-28 21:57:17 +0300

    add brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+
+from ..even_func import question_even
+from ..game import game
+
+
+def main():
+    init_message = 'Answer "yes" if the number is even, otherwise answer "no".'
+    game(init_message, question_even)
+
+
+if __name__ = '__main__':
+    main()
