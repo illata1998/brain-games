4bee5ebc10d3dfe9402049f5ed34b8d10089d5e9
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-03-28 22:35:19 +0300

    add brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from ..calc_func import question_calc
+from ..game import game
+
+
+def main():
+    init_message = 'What is the result of the expression?'
+    game(init_message, question_calc)
+
+if __name__ == '__main__':
+    main()
