fdf98a245722e661a21a54c05e73579c3aeae7df
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-04-03 22:42:44 +0300

    update prime_func.py

--- brain_games/games/prime_func.py
+++ brain_games/games/prime_func.py
@@ -3,10 +3,10 @@ from brain_games.games.question_func import ask_question
 
 
 def is_prime(num):
-    if num <= 1 or num % 2 == 0:
-        return False
     if num == 2:
         return True
+    if num <= 1 or num % 2 == 0:
+        return False
     return all(num % i != 0 for i in range(3, int(num ** 0.5) + 1, 2))
 
 
