6859f9b336c4fc9b5396b4f837e4ffb1c5968ff3
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-04-09 22:43:01 +0300

    add even_and_prime_func.py

---
+++ brain_games/games/even_and_prime_func.py
@@ -0,0 +1,29 @@
+from random import randint
+
+def is_prime(num):
+    if num == 2:
+        return True
+    if num <= 1 or num % 2 == 0:
+        return False
+    return all(num % i != 0 for i in range(3, int(num ** 0.5) + 1, 2))
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def yes_or_no_game(is_func):
+    START = 1
+    FINISH = 100
+    question_num = randint(START, FINISH)
+    correct_answer = 'yes' if is_func(question_num) else 'no'
+    return str(question_num), str(correct_answer)
+
+def even_game():
+    INIT_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".'
+    return INIT_MESSAGE, yes_or_no_game(is_even)
+
+
+def prime_game():
+    INIT_MESSAGE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    return INIT_MESSAGE, yes_or_no_game(is_prime)
