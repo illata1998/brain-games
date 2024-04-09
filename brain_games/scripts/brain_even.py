39670c93ae33f5637bee7633ee37549d356a301d
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-10 00:04:51 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,11 @@
 #!/user/bin/env python3
 
 
-from brain_games.games.even_and_prime_func import even_game
-from brain_games.games.game_func import game_engine
+from brain_games.games.even import even_game
 
 
 def main():
-    game_engine(even_game)
+    even_game()
 
 
 if __name__ == '__main__':
