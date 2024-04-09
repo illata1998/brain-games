4191279eb9a46ffe5cd963bd3c3b4aaeda2c51ec
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 23:52:57 +0300

    Create even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,17 @@
+from random import randint
+from brain_games.games.engine import game_engine
+
+
+INIT_MESSAGE_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def even_answer():
+    START = 1
+    FINISH = 100
+    question_num = randint(START, FINISH)
+    correct_answer = 'yes' if question_num %2 == 0 else 'no'
+    return question_num, correct_answer
+
+
+def even_game():
+    game_engine(even_answer, INIT_MESSAGE_EVEN)
