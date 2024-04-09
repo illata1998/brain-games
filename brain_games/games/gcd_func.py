5981e6a074f7f08e8aa8923777be06604543252c
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 23:01:35 +0300

    Update gcd_func.py

--- brain_games/games/gcd_func.py
+++ brain_games/games/gcd_func.py
@@ -16,4 +16,8 @@ def gcd_game():
     INIT_MESSAGE = 'Find the greatest common divisor of given numbers.'
     num1, num2 = randint(START, FINISH), randint(START, FINISH)
     correct_answer = find_gcd(num1, num2)
-    return INIT_MESSAGE, f'{num1} {num2}', str(correct_answer)
+    return (
+        INIT_MESSAGE,
+        f'{num1} {num2}',
+        str(correct_answer)
+    )
