1f1b8699b7c6970182fc40228e9f6f42b7721607
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-10 00:15:21 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,7 +2,8 @@ from random import randint
 from brain_games.games.engine import game_engine
 
 
-INIT_MESSAGE_PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+INIT_MESSAGE_PRIME = 'Answer "yes" if given number is prime. '\
+    'Otherwise answer "no".'
 
 
 def is_prime(num):
