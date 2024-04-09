51ee05f2eb2135c19cd6cbc7b95ad36f90be6756
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 22:26:23 +0300

    Update progression_func.py

--- brain_games/games/progression_func.py
+++ brain_games/games/progression_func.py
@@ -1,14 +1,14 @@
 from random import randint
-from brain_games.games.question_func import ask_question
 
 
-def progression_game(attempt_count):
+def progression_game():
     START = 1
     FINISH = 100
     MIN_LENGTH = 5
     MAX_LENGTH = 10
     MIN_STEP = 1
     MAX_STEP = 10
+    INIT_MESSAGE = 'What number is missing in the progression?'
     progression = [randint(START, FINISH)]
     progression_length = randint(MIN_LENGTH, MAX_LENGTH)
     step = randint(MIN_STEP, MAX_STEP)
@@ -17,7 +17,4 @@ def progression_game(attempt_count):
     missing_item_index = randint(0, progression_length - 1)
     missing_item = progression[missing_item_index]
     progression[missing_item_index] = '..'
-    if attempt_count == 0:
-        print('What number is missing in the progression?')
-    ask_question(' '.join(map(str, progression)))
-    return str(missing_item)
+    return INIT_MESSAGE, ' '.join(map(str, progression)), str(missing_item)
