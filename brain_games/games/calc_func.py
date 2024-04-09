e4ccb4738350625b51a0aef41304301169851d65
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 22:19:20 +0300

    update calc_func.py

--- brain_games/games/calc_func.py
+++ brain_games/games/calc_func.py
@@ -1,20 +1,18 @@
 from random import randint
-from brain_games.games.question_func import ask_question
 
 
-def calc_game(attempt_count):
+def calc_game():
     START = 1
     FINISH = 10
+    INIT_MESSAGE = 'What is the result of the expression?'
     num1, num2 = randint(START, FINISH), randint(START, FINISH)
-    math_signs = ['+', '-', '*']
-    math_sign_index = randint(0, len(math_signs) - 1)
-    if attempt_count == 0:
-        print('What is the result of the expression?')
-    ask_question(f'{num1} {math_signs[math_sign_index]} {num2}')
-    match math_sign_index:
+    signs = ['+', '-', '*']
+    sign_index = randint(0, len(signs) - 1)
+    match sign_index:
         case 0:
-            return str(num1 + num2)
+            correct_answer = num1 + num2
         case 1:
-            return str(num1 - num2)
+            correct_answer = num1 - num2
         case 2:
-            return str(num1 * num2)
+            correct_answer = num1 * num2
+    return INIT_MESSAGE, f'{num1} {signs[sign_index]} {num2}', str(correct_answer)
