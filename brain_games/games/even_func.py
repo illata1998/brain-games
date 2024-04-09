03b46ca62e3d2e941a54c90bfa296e9194b9eec6
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 22:21:12 +0300

    Update even_func.py

--- brain_games/games/even_func.py
+++ brain_games/games/even_func.py
@@ -1,11 +1,14 @@
-from brain_games.games.yes_or_no_func import yes_or_no_game
+from random import randint
 
 
 def is_even(num):
     return num % 2 == 0
 
 
-def even_game(attempt_count):
-    if attempt_count == 0:
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-    return yes_or_no_game(is_even)
+def even_game():
+    START = 1
+    FINISH = 100
+    INIT_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".'
+    question_num = randint(START, FINISH)
+    correct_answer = 'yes' if is_even(question_num) else 'no'
+    return INIT_MESSAGE, str(question_num), str(correct_answer)
