79c8a9fb845861c6e77aa8e2aa45115cd8fcb3ab
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-10 00:04:25 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,12 +1,11 @@
 #!/user/bin/env python3
 
 
-from brain_games.games.calc_func import calc_game
-from brain_games.games.game_func import game_engine
+from brain_games.games.calc import calc_game
 
 
 def main():
-    game_engine(calc_game)
+    calc_game()
 
 
 if __name__ == '__main__':
