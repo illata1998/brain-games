a538adcb3a9a1aad9d49014f8d329c95ce25df2e
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-09 22:24:07 +0300

    Update gcd_func.py

--- brain_games/games/gcd_func.py
+++ brain_games/games/gcd_func.py
@@ -1,5 +1,4 @@
 from random import randint
-from brain_games.games.question_func import ask_question
 
 
 def find_gcd(num1, num2):
@@ -11,11 +10,10 @@ def find_gcd(num1, num2):
     return num1 + num2
 
 
-def gcd_game(attempt_count):
+def gcd_game():
     START = 1
     FINISH = 100
+    INIT_MESSAGE = 'Find the greatest common divisor of given numbers.'
     num1, num2 = randint(START, FINISH), randint(START, FINISH)
-    if attempt_count == 0:
-        print('Find the greatest common divisor of given numbers.')
-    ask_question(f'{num1} {num2}')
-    return str(find_gcd(num1, num2))
+    correct_answer = find_gcd(num1, num2)
+    return INIT_MESSAGE, f'{num1} {num2}', str(correct_answer)
