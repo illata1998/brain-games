755ee58f2283217a020ac1c84ea765c229dae233
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-16 22:20:20 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,13 +1,9 @@
 from random import randint
 
 
-INIT_MESSAGE_GCD = 'Find the greatest common divisor of given numbers.'
+INIT_MESSAGE = 'Find the greatest common divisor of given numbers.'
 MIN_NUMBER = 1
 MAX_NUMBER = 100
-
-
-number1 = randint(MIN_NUMBER, MAX_NUMBER)
-number2 = randint(MIN_NUMBER, MAX_NUMBER)
 
 
 def find_gcd(a, b):
@@ -19,7 +15,9 @@ def find_gcd(a, b):
     return a + b
 
 
-def gcd_game(number1, number2):
+def generate_round():
+    number1 = randint(MIN_NUMBER, MAX_NUMBER)
+    number2 = randint(MIN_NUMBER, MAX_NUMBER)
     question = f'{number1} {number2}'
     correct_answer = str(find_gcd(number1, number2))
     return question, correct_answer
