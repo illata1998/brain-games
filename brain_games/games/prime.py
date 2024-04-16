a6c266bf2fa2915e0bc8947060172bb4b8e31060
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-16 21:15:35 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,26 +1,24 @@
 from random import randint
-from brain_games.games.engine import game_engine
 
 
 INIT_MESSAGE_PRIME = 'Answer "yes" if given number is prime. '\
     'Otherwise answer "no".'
+MIN_NUMBER = 1
+MAX_NUMBER = 100
 
 
-def is_prime(num):
-    if num == 2:
+number = randint(MIN_NUMBER, MAX_NUMBER)
+
+
+def is_prime(number):
+    if number == 2:
         return True
-    if num <= 1 or num % 2 == 0:
+    if number <= 1 or number % 2 == 0:
         return False
-    return all(num % i != 0 for i in range(3, int(num ** 0.5) + 1, 2))
+    return all(number % i != 0 for i in range(3, int(number ** 0.5) + 1, 2))
 
 
-def prime_answer():
-    START = 1
-    FINISH = 100
-    question_num = randint(START, FINISH)
-    correct_answer = 'yes' if is_prime(question_num) else 'no'
-    return question_num, correct_answer
-
-
-def prime_game():
-    game_engine(prime_answer, INIT_MESSAGE_PRIME)
+def prime_game(number):
+    question = str(number)
+    correct_answer = 'yes' if is_prime(number) else 'no'
+    return question, correct_answer
