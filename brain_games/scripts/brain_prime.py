4b53fecd43e0c770bb16b6f19cbe3803fd20efa5
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-17 20:36:10 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,12 +1,12 @@
 #!/user/bin/env python3
 
 
-from brain_games.games.engine import game_engine
+from brain_games.engine import run_game
 import brain_games.games.prime as prime
 
 
 def main():
-    game_engine(prime)
+    run_game(prime)
 
 
 if __name__ == '__main__':
