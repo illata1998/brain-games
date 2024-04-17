c6c83f708e63fda24160abe398de6acde99506ee
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-17 20:59:35 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,32 +2,33 @@ from random import randint
 
 
 INIT_MESSAGE = 'What number is missing in the progression?'
-MIN_FIRST = 1
-MAX_FIRST = 100
-MIN_STEP = 1
-MAX_STEP = 10
+MIN_INITIAL_TERM = 1
+MAX_INITIAL_TERM = 100
+MIN_COMMON_DIFFERENCE = 1
+MAX_COMMON_DIFFERENCE = 10
 MIN_LENGTH = 5
 MAX_LENGTH = 10
 
 
-def generate_progression(first, step, length):
-    progression = [first]
+def generate_progression(initial_term, common_difference, length):
+    progression = [initial_term]
     for i in range(length - 1):
-        progression.append(progression[i] + step)
+        progression.append(progression[i] + common_difference)
     return progression
 
 
-def remove_item(progression, removed_item_index):
-    removed_item = progression[removed_item_index]
-    progression[removed_item_index] = '..'
-    return ' '.join(map(str, progression)), str(removed_item)
+def convert_progression_into_string(progression, replaced_item_index):
+    replaced_item = progression[replaced_item_index]
+    progression[replaced_item_index] = '..'
+    return ' '.join(map(str, progression)), str(replaced_item)
 
 
 def generate_round():
-    first = randint(MIN_FIRST, MAX_FIRST)
-    step = randint(MIN_STEP, MAX_STEP)
+    initial_term = randint(MIN_INITIAL_TERM, MAX_INITIAL_TERM)
+    common_difference = randint(MIN_COMMON_DIFFERENCE, MAX_COMMON_DIFFERENCE)
     length = randint(MIN_LENGTH, MAX_LENGTH)
-    removed_item_index = randint(0, length - 1)
-    progression = generate_progression(first, step, length)
-    question, correct_answer = remove_item(progression, removed_item_index)
+    replaced_item_index = randint(0, length - 1)
+    progression = generate_progression(initial_term, common_difference, length)
+    question, correct_answer =\
+        convert_progression_into_string(progression,replaced_item_index)
     return question, correct_answer
