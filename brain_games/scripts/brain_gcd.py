8ab6ca252bc935d572fbcb3111aed6618f9d7247
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-17 20:35:36 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,12 @@
 #!/user/bin/env python3
 
 
-from brain_games.games.engine import game_engine
+from brain_games.engine import run_game
 import brain_games.games.gcd as gcd
 
 
 def main():
-    game_engine(gcd)
+    run_game(gcd)
 
 
 if __name__ == '__main__':
