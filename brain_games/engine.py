1af402e4c0bd1bb11460dad1d68acfe6f3f4a824
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-04-17 20:33:21 +0300

    Update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,22 +1,22 @@
 import prompt
 
 
-MAX_ATTEMPT_COUNT = 3
+MAX_ROUND_COUNT = 3
 
 
-def game_engine(game):
-    attempt_count = 0
+def run_game(game):
+    round_count = 0
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.INIT_MESSAGE)
-    while attempt_count < MAX_ATTEMPT_COUNT:
+    while round_count < MAX_ROUND_COUNT:
         question_str, correct_answer = game.generate_round()
         print(f'Question: {question_str}')
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
-            attempt_count += 1
+            round_count += 1
         else:
             print(f"'{answer}' is wrong answer ;(. Correct answer was "
                   f"'{correct_answer}'.")
